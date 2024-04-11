1e1fb1000041f4e76a9bcc419c62fd08bc8dac2a
cyborja <cyborj88@gmail.com>
2024-04-12 01:58:54 +0800

    replace default code with simple components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'primereact/button';     
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Gold from './pages/Gold';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="card flex justify-content-center">
-        <Button label="Check" icon="pi pi-check" />
-      </div>
-    </div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/gold" element={<Gold/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
